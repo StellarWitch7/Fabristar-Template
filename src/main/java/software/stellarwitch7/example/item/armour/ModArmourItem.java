149265a44287c871d77f254fbd6449c72686cd0a
Aurora Dawn <131844170+StellarWitch7@users.noreply.github.com>
2023-05-07 22:01:25 -0400

    Added ModArmourItem.java

---
+++ src/main/java/software/stellarwitch7/example/item/armour/ModArmourItem.java
@@ -0,0 +1,11 @@
+package software.stellarwitch7.ellieguard.item.armour;
+
+import net.minecraft.entity.EquipmentSlot;
+import net.minecraft.item.ArmorItem;
+import net.minecraft.item.ArmorMaterial;
+
+public class ModArmourItem extends ArmorItem {
+	public ModArmourItem(ArmorMaterial material, EquipmentSlot slot, Settings settings) {
+		super(material, slot, settings);
+	}
+}
